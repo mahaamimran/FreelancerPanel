b1bfdf69fa18cdb6bb39851ac9640388979566ff
Maham <mahaamimran@gmail.com>
2024-12-08 19:34:25 +0500

    content

--- frontend-freelancer-panel/src/components/JobContent.jsx
+++ frontend-freelancer-panel/src/components/JobContent.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import JobCard from "./JobCard";
+import JobCard from "./Jobcard";
 import Pagination from "./Pagination";
 
 const JobContent = ({ jobs, currentPage, jobsPerPage, onPageChange }) => {
